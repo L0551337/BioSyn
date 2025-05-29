624c6464e64be6875fa1cb469531d0ae276c2c28
Enzo <enzolsts@gmail.com>
2025-05-29 14:17:07 -0300

    Colocando especificidade de porta como comentario por ser raramente necessaria

--- BioSyn/config.py
+++ BioSyn/config.py
@@ -2,4 +2,5 @@ MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = '@478517EnzoLust'
 MYSQL_DB = 'banco_eden'
-MYSQL_PORT = 3308
+#Especificidade de porta para caso a instancia mysql n esteja hospedada na porta 3306 (meu caso)
+#MYSQL_PORT = 3308
